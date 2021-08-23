e15d21c98ca1caedecdc30bd52b80333365254e7
cnsd <chris.sanchees@hotmail.com>
2021-08-23 12:37:42 -0300

    Kafka Producer

---
+++ src/main/java/com/live/kafka/producer/producer/CarProducer.java
@@ -0,0 +1,31 @@
+package com.live.kafka.producer.producer;
+
+import com.live.kafka.producer.controller.CarDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
+@Slf4j
+public class CarProducer {
+
+//    private static final Logger logger;
+    private final String topic;
+    private final KafkaTemplate<String, CarDTO> kafkaTemplate;
+
+    public CarProducer(@Value("${topic.name}") String topic, KafkaTemplate<String, CarDTO> kafkaTemplate) {
+        this.topic = topic;
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void send(CarDTO carDTO) {
+        kafkaTemplate.send(topic, carDTO).addCallback(
+                success -> log.info("Message send" + success.getProducerRecord().value()),
+                failure -> log.info("Message failure" + failure.getMessage())
+        );
+    }
+}
